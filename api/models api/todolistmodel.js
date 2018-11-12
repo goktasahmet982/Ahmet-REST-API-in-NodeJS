a38e3791856dabebd7b29bcb60d746039983692e
Ahmet <goktasahmet982@gmail.com>
2018-11-12 14:25:27 +0200

    new

---
+++ api/models api/todolistmodel.js
@@ -0,0 +1,22 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var TaskSchema = new Schema({
+    name : { //first column
+        type : String,
+        required : 'Kindly enter task name'
+    },
+    Created_date : { //second column
+        type : Date,
+        default : Date.now
+    },
+    status : { //third column
+        type : [{
+            type : String,
+            enum : ['pending', 'ongoing', 'completed']
+        }],
+        default : ['pending']
+    }
+});
+
